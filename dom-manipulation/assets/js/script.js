90a850fe59b9dfc837e859ca3e5ac4d432ea8be9
jhdavidesouza <jhdavidesouza@gmail.com>
2022-01-22 11:03:17 -0300

    other 2 projects added

---
+++ dom-manipulation/assets/js/script.js
@@ -0,0 +1,12 @@
+console.log("This is a script file!")
+
+/* there is a way to change the elements properties
+    OBS: IT WORKS ONLY ON THE BROWSER CONSOLE
+    by typing
+        document.getElementsByTagName('p')[0]
+    than you'll see
+        an HTML Collection
+    declare it into a variable
+        var text = document.getElementsByTagName('p')[0]
+    than change the color to blue
+        text.style.color = 'blue' */
