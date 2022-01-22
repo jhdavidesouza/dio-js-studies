20e924353de9d0a296ac5203c9102467963e5505
jhdavidesouza <jhdavidesouza@gmail.com>
2022-01-22 13:02:54 -0300

    added a modified version of counter enhanced (with conditional)

--- counter-enhanced/assets/js/script.js
+++ counter-enhanced/assets/js/script.js
@@ -1,9 +1,10 @@
 var currentNumberHTML = document.getElementById('currentNumber');//we want to change the content inside the HTML tag
 var currentNumber = 0; //initiating the same way inside the HTML content
 /* this is how you call addEventListener */
-var decrementButton = document.getElementById('decrementButton');
+var decrementButton = document.getElementById('decrementButton');//classifying the variables with the elements IDs
 var incrementButton = document.getElementById('incrementButton');
 
+//adding addEventListener method to the classified variables
 incrementButton.addEventListener('click', () => {//function to increase the number by 1
     currentNumber++; //incrementer
     currentNumberHTML.innerHTML = currentNumber;//the HTML element gets the value of currentNumber and assign it into itself
