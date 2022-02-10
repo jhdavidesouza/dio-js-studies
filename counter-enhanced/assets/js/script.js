dde240ff5082af2247493ddc26aa78b728a3a0b2
jhdavidesouza <jhdavidesouza@gmail.com>
2022-02-10 13:01:32 -0300

    altering the way the method is called

--- counter-enhanced/assets/js/script.js
+++ counter-enhanced/assets/js/script.js
@@ -1,16 +1,22 @@
 var currentNumberHTML = document.getElementById('currentNumber');//we want to change the content inside the HTML tag
 var currentNumber = 0; //initiating the same way inside the HTML content
 /* this is how you call addEventListener */
-var decrementButton = document.getElementById('decrementButton');//classifying the variables with the elements IDs
-var incrementButton = document.getElementById('incrementButton');
 
 //adding addEventListener method to the classified variables
-incrementButton.addEventListener('click', () => {//function to increase the number by 1
+var decrementButton = document.getElementById('decrementButton').addEventListener('click', decreaseNumber);//classifying the variables with the elements IDs
+var incrementButton = document.getElementById('incrementButton').addEventListener('click', increaseNumber);
+
+
+function increaseNumber(){//function to increase the number by 1
     currentNumber++; //incrementer
     currentNumberHTML.innerHTML = currentNumber;//the HTML element gets the value of currentNumber and assign it into itself
-});
+}
 
-decrementButton.addEventListener( 'click', () => { //function to decrease the number by 1
+function decreaseNumber(){//function to decrease the number by 1
     currentNumber--; //decrementer
     currentNumberHTML.innerHTML = currentNumber; //the HTML element gets the value of currentNumber and assign it into itself
-});
+}
+
+
+
+
