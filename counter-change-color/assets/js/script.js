cdd72d7ee7cdb748b7c171871315ebb3257f2d34
jhdavidesouza <jhdavidesouza@gmail.com>
2022-02-10 13:00:50 -0300

    altering the way the method addEventListener is called

--- counter-change-color/assets/js/script.js
+++ counter-change-color/assets/js/script.js
@@ -1,22 +1,19 @@
 var currentNumberHTML = document.getElementById('currentNumber');//we want to change the content inside the HTML tag
+currentNumberHTML.style.color = "blue"; //initialize the numbers already in blue
 var currentNumber = 0; //initiating the same way inside the HTML content
+
 /* this is how you call addEventListener */
-var decrementButton = document.getElementById('decrementButton');//classifying the variables with the elements IDs
-var incrementButton = document.getElementById('incrementButton');
 
 //calling the methods to each element and apply these functions 
-incrementButton.addEventListener('click', increase);
-decrementButton.addEventListener('click', decrease);
+var decrementButton = document.getElementById('decrementButton').addEventListener('click', decrease);//classifying the variables with the elements IDs
+var incrementButton = document.getElementById('incrementButton').addEventListener('click', increase);;
+
 
 function increase(){
     currentNumber++;//increasing the value by 1
-    if(currentNumber <= 10){//stops modifying the number, if it's over 10
-        currentNumberHTML.innerHTML = currentNumber;
-    } else if(currentNumber > 10){//if it's 11, it will turn back into 10 again.
-        currentNumber--;
-    }
+    currentNumberHTML.innerHTML = currentNumber;
     if(currentNumber >= 0){
-        currentNumberHTML.style.color = 'inherit';
+        currentNumberHTML.style.color = 'blue';
     }
 }
 
