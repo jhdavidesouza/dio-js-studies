2f43055430df0d3ca59c1665bce52cda6d325a0b
jhdavidesouza <jhdavidesouza@gmail.com>
2022-02-12 10:29:25 -0300

    applying form tag and making it more semantic

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -1,12 +1,1 @@
-let addButton = document.getElementById("add-task-button").addEventListener("click", addTask);
-let addLabel = document.getElementById("task-input");
 
-
-function addTask(){
-    let newLabel = document.createElement("label");
-    let newInput = document.createElement("input");
-    newInput.setAttribute("type", "checkbox");
-
-    let toDoList = document.getElementById("todo-list");
-    toDoList.appendChild(newInput);
-}
