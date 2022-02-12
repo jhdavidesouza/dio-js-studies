926f0c45d71b17153f7edf486e4ca7c0496fa152
jhdavidesouza <jhdavidesouza@gmail.com>
2022-02-12 11:23:31 -0300

    making JS script work on to-do-list

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -1,29 +1,35 @@
-//creating variable that gets button value
-let button = document.getElementById("add-task-button");
+const referenceList = document.getElementById("todo-list");//creating a variable that gets the reference from which we will append the recently created elements above
+const ourForm = document.getElementById("form");
 
-//calling the method, so when I click on the button
-//it'll start the action
-button.addEventListener("click", addTask);
-
-function addTask(){//function created to add a task
-
-    //we need to get the text that was written inside 
-    //input tag
-    let textFromInput = document.getElementById("task-input").value;
-
-    //then, we need to create an input 'checkbox' and a label
-    let newInput = document.createElement("input");//here we create a new input element
-    //now, let's set the attribute of this element, so it can be a type checkbox input
-    newInput.setAttribute("type", "checkbox");//setting the type
-    newInput.setAttribute("id", textFromInput);//setting the ID that will help us with the label
-
-    let newLabel = document.createElement("label");//creating a new label element
-    newLabel.setAttribute("for", textFromInput);//setting the attribute for, so it can be directly related to its input element
-    newLabel.innerHTML = textFromInput;//the text inside the HTML label tag will be the same thing the user wrote inside the text type input
-
-    
-
-
+ourForm.onsubmit = function (x){
+    x.preventDefault(); //search more about this method
+    const inputUserWritesInside = document.getElementById("task-input");
+    addTask(inputUserWritesInside.value);
+    form.reset(); //search more about this method and why does it help the solution make it work
 
 }
 
+function addTask(writtenText){//function created to add a task
+
+    //then, we need to create an input 'checkbox' and a label
+    const newInput = document.createElement("input");//here we create a new input element
+    //now, let's set the attribute of this element, so it can be a type checkbox input
+    newInput.setAttribute("type", "checkbox");//setting the type
+    newInput.setAttribute("id", writtenText);//setting the ID that will help us with the label
+    newInput.setAttribute("name", writtenText);//setting the NAME attribute that will help us with the label
+
+    const newLabel = document.createElement("label");//creating a new label element
+    newLabel.setAttribute("for", writtenText);//setting the attribute for, so it can be directly related to its input element
+    newLabel.appendChild(document.createTextNode(writtenText));//the text inside the HTML label tag will be the same thing the user wrote inside the text type input
+
+    //to wrap it all we need a div that serves us to put the new Input and the new Label created inside
+    const newContainer = document.createElement("div");
+    newContainer.classList.add("task-item");
+    newContainer.appendChild(newInput);
+    newContainer.appendChild(newLabel);
+
+    referenceList.appendChild(newContainer);//inserting the new container into the list
+
+}
+
+//it works... finally
