588b4ba0bd9bf30991ccfb7a3e16ab2a2aa8bc85
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-16 01:06:09 -0300

    an array example and some methods to manipulate it

--- syntax-js/objectsAndArrays/arrays.js
+++ syntax-js/objectsAndArrays/arrays.js
@@ -6,3 +6,32 @@
  *  how to manipulate arrays
  * 
  */
+
+/*
+  Vetores or Arrays
+  
+  Kind of list that you can add a lot of values of any types.
+  A box inside a box.
+
+*/
+
+// this is how you can declare an array in JS
+
+var arrayExample = ["hello", true, 34.5, [true, false, false]];
+console.log(arrayExample[3]);
+
+//We can manipulate arrays through methods
+
+//You'll learn a lot of methods to manipulate arrays in your path, but now let's stick with some of them:
+
+/*
+  forEach() - itera um array;
+  push() - adiciona item no final do array;
+  pop() - remove item no final do array;
+  shift() - remove item no início do array;
+  unshift() - adiciona item no início do array;
+  indexOf() - retorna o índice de um valor;
+
+*/
+
+
