a765e2d1480eee9957ed58abb64003ba353a9b38
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-17 00:59:08 -0300

    update if.js

--- syntax-js/conditional-structures/if.js
+++ syntax-js/conditional-structures/if.js
@@ -1,1 +1,32 @@
+// IF structure is a consitional decision making structure
 
+//something will happen IF another thing has happened
+
+var value = 9;
+
+if ( value >= 10 ){
+    console.log("Value is over or equal to 10");
+} else { //another action will happen if the first condition is not true
+    console.log("Value is lower than 10");
+}
+
+//there's the possibility of having more than 1 condition
+
+if ( value > 10 ){
+    console.log("Value is over 10");
+} else if ( value == 10 ){
+    console.log("Value is 10");
+} else {
+    console.log("Value is lower than 10");
+}
+
+//we can nest ifs inside others
+
+if () {
+  if () {
+  }
+}
+
+// there's the ternary operator too, where you can write an if condition in one single line
+
+value > 10 ? value == 10 ? console.log("Value is 10") : console.log("Value is lower than 10");
