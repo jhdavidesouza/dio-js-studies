4211d366f558e70326dea2ac1eb5c71257875c90
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-16 23:55:17 -0300

    completing info about objects and some examples

--- syntax-js/objectsAndArrays/objects.js
+++ syntax-js/objectsAndArrays/objects.js
@@ -4,5 +4,30 @@
     In this class we will know:
     * what objects are
     * how to unravel objects
-
 */
+
+//Objects are data that have values and properties
+// you declare an object in JS as below:
+
+var dog = {
+    size: "big",
+    color: "black",
+    behavior: console.log("woof woof!")
+};
+
+// So the object "dog" has 3 properties --> size, color and behavior
+//These properties have their own values
+
+/*
+    Each object can be manipulated by
+    attributing the value of its properties to a variable
+*/
+
+//Like this
+
+var puppy = dog.color;
+
+//we can do what we call destructuring of an object
+//doing this:
+
+var {size, color} = dog;
