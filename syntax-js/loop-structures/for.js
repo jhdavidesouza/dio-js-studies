bd8d6b1826b4b797f0c061dd8e754d1d9b5680ae
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-17 01:14:35 -0300

    Create for.js

---
+++ syntax-js/loop-structures/for.js
@@ -0,0 +1,14 @@
+//for is a loop structure
+
+/*
+a loop structure is a structure
+that repeats itself until a condition is satisfied
+*/
+
+//in for case, when the condition is not true anymore, the loop stops
+
+var counting = 0;
+
+for ( counting; counting <= 10; counting++ ){
+    console.log(counting);
+}
