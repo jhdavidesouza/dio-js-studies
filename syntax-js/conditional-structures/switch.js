0edf6d79f9852b2be9dc822189590fd0d3b8b899
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-17 01:10:27 -0300

    update swtich.js

--- syntax-js/conditional-structures/switch.js
+++ syntax-js/conditional-structures/switch.js
@@ -12,3 +12,18 @@ switch ( expression ) {
     default IN CASE NO EXPRESSIONS ARE TRUE :
         action;
 }
+
+//for example
+
+var choice = 1;
+
+switch (choice){
+    case 1 :
+        console.log("It's 1");
+        break;
+    case 2 :
+        console.log("It's 2");
+        break;
+    default :
+        console.log("It's neither 2 nor 1");
+}
