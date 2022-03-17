76e1f8c43a5394475effa0b1f65977aa5241b388
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-17 01:02:02 -0300

    create switch.js

---
+++ syntax-js/conditional-structures/switch.js
@@ -0,0 +1,14 @@
+//switch is another conditional decision structure
+
+//it has a different syntax
+
+switch ( expression ) {
+    case EXPRESSION :
+        action;
+        break;
+    case OTHER EXPRESSION :
+        action;
+        break;
+    default IN CASE NO EXPRESSIONS ARE TRUE :
+        action;
+}
