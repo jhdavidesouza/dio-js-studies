398787513f2e8cf44e911d74b0a748f2dcc9796d
Jhonatas "Tas" Davi de Souza <87619963+jhdavidesouza@users.noreply.github.com>
2022-03-17 01:20:30 -0300

    update for.js

--- syntax-js/loop-structures/for.js
+++ syntax-js/loop-structures/for.js
@@ -12,3 +12,22 @@ var counting = 0;
 for ( counting; counting <= 10; counting++ ){
     console.log(counting);
 }
+
+//let's read that syntax above
+
+/*
+        so FOR loops can initialize with a variable (global or local) or not, but always follows 3 processes
+        initializer ; condition ; increment/decrement
+        
+        the actions inside the FOR loop will repeat itself until the condition is NOT TRUE anymore
+*/
+
+
+for ( counting; counting >= 0; counting-- ){
+    if ( counting > 0 ){
+        console.log("ainda é maior que zero");
+    } else if ( counting == 0 ){
+        console.log("é zero");
+    }
+}
+console.log("menor que zero");
